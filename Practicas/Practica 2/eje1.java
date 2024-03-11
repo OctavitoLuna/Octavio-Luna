46d72636a9fea2c7d167aa7997613186e054e0a3
Octavio <marco.octavio.luna@gmail.com>
2024-03-11 18:33:42 -0400

    3: Practica 2, ejercicio 1 completo y cargado de los demas

---
+++ Practicas/Practica 2/eje1.java
@@ -0,0 +1,46 @@
+public class eje1{
+    public int a;
+    public int b;
+
+    public eje1(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sumar(){
+        return getNum1() + getNum2();
+    }
+    public int restar(){
+        return getNum1() - getNum2();
+    }
+    public int multiplicar(){
+        return getNum1() * getNum2();
+    }
+    public double dividir(){
+        double res = 0;
+        if(b != 0){
+            res = getNum1() / getNum2();
+        }
+        else{
+            res = 0;
+        }
+        return res;
+    }
+
+
+
+
+
+    public void setNum1(int a){
+        this.a = a;
+    }
+    public int getNum1(){
+        return a;
+    }
+    public void setNum2(int b){
+        this.b = b;
+    }
+    public int getNum2(){
+        return b;
+    }
+}
